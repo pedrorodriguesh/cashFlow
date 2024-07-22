3ba842d89df4b97ba85e52274f602f00ac8ad016
Pedro Henrique Rodrigues <pedrorodrigueshdev@gmail.com>
2024-07-22 19:45:46 -0300

    validating errors

--- src/cashFlow.API/Controllers/ExpensesController.cs
+++ src/cashFlow.API/Controllers/ExpensesController.cs
@@ -1,6 +1,6 @@
 using cashFlow.Application.UseCases.Expenses.Create;
 using cashFlow.Communication.Requests;
-
+using cashFlow.Communication.Responses;
 using Microsoft.AspNetCore.Mvc;
 
 namespace cashFlow.API.Controllers
@@ -13,11 +13,33 @@ namespace cashFlow.API.Controllers
         [HttpPost]
         public IActionResult Create([FromBody] RequestCreateExpenseJson request)
         {
-            var useCase = new CreateExpenseUseCase();
+            try
+            {
+                var useCase = new CreateExpenseUseCase();
 
-            var response = useCase.Execute(request);
-            
-            return Created(string.Empty, response);
+                var response = useCase.Execute(request);
+
+                return Created(string.Empty, response);
+            }
+            catch (ArgumentException e)
+            {
+                var errorResponse = new ResponseErrorJson
+                {
+                    ErrorMessage = e.Message
+                };
+
+                return BadRequest(errorResponse);
+            }
+            catch
+            {
+                var errorResponse = new ResponseErrorJson
+                {
+                    ErrorMessage = "Internal Server Error"
+                };
+                
+                
+                return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
+            }
         }
     }
 }
---
+++ src/cashFlow.Communication/Responses/ResponseErrorJson.cs
@@ -0,0 +1,6 @@
+namespace cashFlow.Communication.Responses;
+
+public class ResponseErrorJson
+{
+    public required string ErrorMessage { get; set; } = string.Empty;
+}
