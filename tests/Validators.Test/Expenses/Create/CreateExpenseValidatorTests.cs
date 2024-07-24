b0bff2d3c92ad7a323511123ef3404d64d7b1cc9
Pedro Henrique Rodrigues <pedrorodrigueshdev@gmail.com>
2024-07-24 20:00:25 -0300

    first error test

--- tests/Validators.Test/Expenses/Create/CreateExpenseValidatorTests.cs
+++ tests/Validators.Test/Expenses/Create/CreateExpenseValidatorTests.cs
@@ -1,6 +1,7 @@
 using cashFlow.Application.UseCases.Expenses.Create;
 using cashFlow.Communication.Enums;
 using cashFlow.Communication.Requests;
+using cashFlow.Exception;
 using CommonTestsUtils.Requests;
 using FluentAssertions;
 
@@ -20,5 +21,22 @@ public class CreateExpenseValidatorTests
 
         //Assert
         result.IsValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Error_Title_Empty()
+    {
+        //Arrange
+        var validator = new CreateExpenseValidator();
+        var request = RequestCreateExpenseJsonBuilder.Build();
+        request.Title = string.Empty;
+        
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should()
+            .ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.TITLE_REQUIRED));
     }
 }
