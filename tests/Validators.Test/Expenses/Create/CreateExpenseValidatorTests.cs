f91d3f5d9ccb9be08098b740ce354d8b5ee22a5f
Pedro Henrique Rodrigues <pedrorodrigueshdev@gmail.com>
2024-07-24 21:09:49 -0300

    all tests done

--- tests/Validators.Test/Expenses/Create/CreateExpenseValidatorTests.cs
+++ tests/Validators.Test/Expenses/Create/CreateExpenseValidatorTests.cs
@@ -39,4 +39,59 @@ public class CreateExpenseValidatorTests
         result.Errors.Should()
             .ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.TITLE_REQUIRED));
     }
+    
+    [Fact]
+    public void Error_Date_Future()
+    {
+        //Arrange
+        var validator = new CreateExpenseValidator();
+        var request = RequestCreateExpenseJsonBuilder.Build();
+        request.Date = DateTime.UtcNow.AddDays(2);
+        
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should()
+            .ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.DATE_MUST_BE_IN_FUTURE));
+    }
+    
+    [Fact]
+    public void Error_Payment_Type_Invalid()
+    {
+        //Arrange
+        var validator = new CreateExpenseValidator();
+        var request = RequestCreateExpenseJsonBuilder.Build();
+        request.PaymentType = (PaymentType)700;
+        
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should()
+            .ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.PAYMENT_TYPE_INVALID));
+    }
+    
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(-2)]
+    [InlineData(-7)]
+    public void Error_Amount_Invalid(decimal amount)
+    {
+        //Arrange
+        var validator = new CreateExpenseValidator();
+        var request = RequestCreateExpenseJsonBuilder.Build();
+        request.Amount = amount;
+        
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should()
+            .ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO));
+    }
 }
