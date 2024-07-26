bc97d5a4322c4ecf84d746bce16d6b9ccab3f6fc
Pedro Henrique Rodrigues <pedrorodrigueshdev@gmail.com>
2024-07-26 20:50:44 -0300

    refact: changing appDbContext

--- src/cashFlow.Infrastructure/DataAccess/Repositories/ExpensesRepository.cs
+++ src/cashFlow.Infrastructure/DataAccess/Repositories/ExpensesRepository.cs
@@ -3,12 +3,10 @@ using cashFlow.Domain.Repositories.Expenses;
 
 namespace cashFlow.Infrastructure.DataAccess.Repositories;
 
-internal class ExpensesRepository : IExpensesRepository
+internal class ExpensesRepository(CashFlowDbContext dbContext) : IExpensesRepository
 {
     public void Create(Expense expense)
     {
-        var dbContext = new CashFlowDbContext();
-        
         dbContext.Expenses.Add(expense);
         dbContext.SaveChanges();
     }
--- src/cashFlow.Infrastructure/DependencyInjectionExtension.cs
+++ src/cashFlow.Infrastructure/DependencyInjectionExtension.cs
@@ -1,4 +1,5 @@
 using cashFlow.Domain.Repositories.Expenses;
+using cashFlow.Infrastructure.DataAccess;
 using cashFlow.Infrastructure.DataAccess.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,6 +9,16 @@ public static class DependencyInjectionExtension
 {
     public static void AddInfrastructure(this IServiceCollection servicesCollection)
     {
+        AddDbContext(servicesCollection);
+        AddRepositories(servicesCollection);
+    }
+
+    private static void AddRepositories(IServiceCollection servicesCollection)
+    {
         servicesCollection.AddScoped<IExpensesRepository, ExpensesRepository>();
+    }
+    private static void AddDbContext(IServiceCollection servicesCollection)
+    {
+        servicesCollection.AddDbContext<CashFlowDbContext>();
     }
 }
